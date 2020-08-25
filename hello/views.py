d935704de0069fe416847fcc7246b1442fba428a
vlhvu <70124551+vlhvu@users.noreply.github.com>
2020-08-25 15:47:59 +0700

    up1

--- hello/views.py
+++ hello/views.py
@@ -35,14 +35,14 @@ def decode_payload(request):
 def chatwork_webhook(request):
     payload = decode_payload(request)
     messageChat = payload["webhook_event"]["body"]
-    messageChat1 = messageChat.replace("[To:5130876]Bot_Translate","")
+    #messageChat1 = messageChat.replace("[To:5130876]Bot_Translate","")
 
     translator = Translator()
-    lang = detect(messageChat1)
+    lang = detect(messageChat)
     locale = "vi"
     if lang == "vi":
         locale = "ja"
-    translated = translator.translate(messageChat1, src=lang, dest=locale)
+    translated = translator.translate(messageChat, src=lang, dest=locale)
     #Send Data back to chatwork
     client = ch.ChatworkClient('fd0602c43dd83cae39e7ebfb08d5793d')
     # get message from room 1234
