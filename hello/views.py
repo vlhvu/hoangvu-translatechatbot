b70c252533d0ac68823476846df39dc2fc49e68c
vlhvu <70124551+vlhvu@users.noreply.github.com>
2020-08-29 19:28:46 +0700

    day la commit

--- hello/views.py
+++ hello/views.py
@@ -30,6 +30,7 @@ def decode_payload(request):
 def chatwork_webhook(request):
     payload = decode_payload(request)
     messageChat = payload["webhook_event"]["body"]
+    # print(messageChat)
     print(messageChat)
     # messageChat1 = messageChat.replace("[To:5130876]Bot_Translate","")
 
