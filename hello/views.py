c1f99215b32c493cbe0bf484613ba6f3e1e333d9
vlhvu <70124551+vlhvu@users.noreply.github.com>
2020-08-31 13:37:08 +0700

    comit lan cuoi4

--- hello/views.py
+++ hello/views.py
@@ -39,7 +39,6 @@ def chatwork_webhook(request):
     print('request from client')
     print(payload)
     #test name
-    messageChat = 'hwe'
     messageChat = re.sub('[a-z]*@', 'ABC@', messageChat)
 
     accountId = payload["webhook_event"]["account_id"]
